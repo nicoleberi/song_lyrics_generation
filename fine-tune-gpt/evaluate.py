cc28dd22239a41f4322b532933662bd4e376dd6e
nicoleberi <nikaberi@gmail.com>
2024-04-17 20:47:01 -0400

    updated code for evaluating GPT

--- fine-tune-gpt/evaluate.py
+++ fine-tune-gpt/evaluate.py
@@ -7,17 +7,30 @@ from datasets import Dataset
 import torch
 
 def test():
-    # to load
-    model = GPT2LMHeadModel.from_pretrained('./gpt2-fine-tuned')
-    tokenizer = GPT2Tokenizer.from_pretrained('./gpt2-fine-tuned')
+    # to load fine-tuned model
+    model = GPT2LMHeadModel.from_pretrained('./fine-tuned_model_new')
+    tokenizer = GPT2Tokenizer.from_pretrained('./fine-tuned_model_new')
+
+    # pre-trained model
+    # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    # model = GPT2LMHeadModel.from_pretrained('gpt2')
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
-
-    prompt = "Write the lyrics to a love song."
-    input_tok = tokenizer(prompt, return_tensors='pt', padding=True).to(device)
-    tokenizer.pad_token = tokenizer.eos_token
-
-    result = model.generate(input_ids=input_tok["input_ids"], max_length=100, do_sample=True, top_p=0.9, temperature=0.7, pad_token_id=tokenizer.pad_token_id)
+    
+    #prompt = "Write a song lyrics that starts with the following text: I love you more everyday because"
+    #prompt = "Write a song lyrics that starts with the following text: It's over now because we couldn't"
+    #prompt = "Write a song lyrics that starts with the following text: I've been so sad ever since"
+    #prompt = "Write a song lyrics that starts with the following text: I've been so happy ever since"
+    #prompt = "Write a song lyrics that starts with the following text: Once upon a time we were"
+    #prompt = "Write a song lyrics that starts with the following text: nicki minaj yo now we gonna"
+    #prompt = "Write a song lyrics that starts with the following text: I want to go somewhere far"
+    #prompt = "Write a song lyrics that starts with the following text: girl this song is for you"
+    #prompt = "Write a song lyrics that starts with the following text: I want to change the world"
+    prompt = "Write a song lyrics that starts with the following text: I can't wait for summer to"
+    input_tok = tokenizer(prompt, return_tensors='pt').to(device)
+    
+    result = model.generate(input_ids=input_tok["input_ids"], attention_mask=input_tok["attention_mask"], max_length=145, do_sample=True, top_p=0.8, temperature=0.7, repetition_penalty=1.2)
 
     result_text = tokenizer.decode(result[0], skip_special_tokens=True)
     return result_text
